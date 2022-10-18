550c2674aa05a9fdcef98ed3350575a88beacd12
werifum <weri.fum@gmail.com>
2022-10-18 12:03:35 +0330

    sdsdsd

---
+++ qdwb/evapotranspiration.py
@@ -0,0 +1,34 @@
+
+
+
+
+
+# Potential Evapotranspiration
+class PotentialEvapotranspiration():
+    
+    def __init__(self):
+        pass
+    
+    
+    def hargreaves_samani():
+        pass
+    
+    
+    def fao_penman_monteith():
+        pass
+
+
+
+# Actual Evapotranspiration
+class ActualEvapotranspiration():
+    
+    def __init__(self):
+        pass
+
+
+
+# Crop Evapotranspiration
+class CropEvapotranspiration():
+    
+    def __init__(self):
+        pass
